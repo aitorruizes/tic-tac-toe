1a67c356df84f0f1e3db13ef4c518f170071e211
Aitor Ruiz <aragra.external@axa-assistance.us>
2024-08-19 15:54:41 -0300

    feat: add 'print_board()' function
    - Added 'print_board()' prototype in 'game.h'
    - Implemented 'print_board()' in 'game.c'
    - Function prints the board
    - Integrated 'print_board()' into 'handle_menu()' in 'game.c'

--- include/game.h
+++ include/game.h
@@ -6,3 +6,4 @@ void print_menu();
 int get_menu_option();
 void handle_menu(int menuOption);
 void initialize_board(char board[BOARD_SIZE][BOARD_SIZE]);
+void print_board(char board[BOARD_SIZE][BOARD_SIZE]);
