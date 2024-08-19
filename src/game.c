fe96e997f99a4930c32c0e3345adfae7e2476c24
Aitor Ruiz <aragra.external@axa-assistance.us>
2024-08-19 14:56:29 -0300

    feat: add 'print_menu()'
    - Created 'include' folder for header files
    - Created 'game.h' in 'include' folder
    - Added 'print_menu()' prototype into 'game.h'
    - Added '#pragma once' to 'game.h' to prevent multiple inclusions
    - Created 'game.c' in 'src' folder
    - Implemented 'print_menu()' function into 'game.c'
    - Function displays a menu with a title and options

---
+++ src/game.c
@@ -0,0 +1,16 @@
+#include "../include/game.h"
+#include <stdio.h>
+
+void print_menu()
+{
+  printf("+---------------------------------------+\n");
+  printf("|                                       |\n");
+  printf("|              TIC-TAC-TOE              |\n");
+  printf("|                                       |\n");
+  printf("+---------------------------------------+\n");
+  printf("|                                       |\n");
+  printf("|  [1] - Player against another player  |\n");
+  printf("|  [2] - Player against AI              |\n");
+  printf("|                                       |\n");
+  printf("+---------------------------------------+\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
+#include "../include/game.h"
 
 int main(void)
 {
+  print_menu();
+  
   return 0;
 }
